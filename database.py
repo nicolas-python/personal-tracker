30e2b332c507d072bbe73c864a1add822751e923
Nicolas <nicolasmader001@gmail.com>
2026-09-25 15:34:05 +0200

    feat(register): add registration page

--- database.py
+++ database.py
@@ -19,32 +19,5 @@ def create_database():
     connection.commit()
     connection.close()
 
-def add_test_user():
-    connection = sqlite3.connect(DATABASE)
-
-    cursor = connection.cursor()
-
-    cursor.execute("""
-        INSERT INTO users (username, password_hash)
-        VALUES (?, ?)
-    """, ("testuser", "testpass"))
-
-    connection.commit()
-    connection.close()
-
-def show_users():
-    connection = sqlite3.connect(DATABASE)
-
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT * FROM users")
-
-    users = cursor.fetchall()
-
-    print(users)
-
-    connection.close()
 
 create_database()
-add_test_user()
-show_users()
--- fitness_app.py
+++ fitness_app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("login.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 @app.route("/login", methods=["POST"])
 def login():
     return "Login wurde abgeschickt"
